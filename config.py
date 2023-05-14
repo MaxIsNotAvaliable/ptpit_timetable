0741de8bedae150f0d31f6c5fe9295b885b6a3f3
MaxIsNotAvaliable <128700843+MaxIsNotAvaliable@users.noreply.github.com>
2023-05-14 12:40:31 +0300

    Update config.py
    Comments for config.py.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+# Для того чтобы поднять бота можно переписать базу (database.py) и файл конфига или заполнить данные своего сервера.
+
+
+
+# Для отладки бота, без прерываний его работы, использовались 2 отдельных бота в телеграм.
+# Получить токен можно у bot father.
 TOKEN_RELEASE   =    'token1'
 TOKEN_DEBUG     =    'token2'
 
@@ -8,16 +14,21 @@ def IsBotInDebug():
 
 alphabet = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"]
 
+# Здесь был список админов бота. Он был сделан в целях отладки и просмотра статистики.
+# Айди пользователей брался из message.chat.id.
 testers = [
 # uid
 ]
 
+# Бот имел собственную базу данных на отдельном сервере и для доступа к базе был написан интерфейс на php.
 user_agent = "ua"
-tgbot_url = "bd"
-bd_request = f"{tgbot_url}/1"
-bd_url = f"{tgbot_url}/2"
+# tgbot_url - представляло собой основную ссылку для отправки запросов.
+tgbot_url = "your_database.com/bot"
+# bd_request - запрос для получения базы данных в json формате.
+bd_request = f"{tgbot_url}/get"
+# bd_url - запрос для отправки данных на сервер.
+bd_url = f"{tgbot_url}/send"
 
-table_name = "users_test"
 
 class pair:
     Name = "Пары нет"
@@ -46,7 +57,8 @@ class pair:
 class DirName:
     uid : int
     courseYr : str
-        
+
+# Это чисто в рофл скрафчено было, бот отвечал стикерами, если в сообщении пользователя было что-то из ViolentWords.
 SwearStr = [
     'CAACAgEAAxkBAAICP2G3TPdddbdf0oLGyu9oT2Zbz7ORAAJuIwACePzGBZ8lB6a5wmkiIwQ',
     'CAACAgEAAxkBAAICQWG3TQWFxQzmz20PSoxWsdSCGlBYAAJpIwACePzGBQ9aaXLhnDlgIwQ',
